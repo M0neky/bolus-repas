a32b09b3a030178941662128575c660eca12cc96
M0neky <vincent.bourdin76@hotmail.fr>
2026-08-22 10:37:29 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "bolus-repas-v5";
+const CACHE_NAME = "bolus-repas-v11";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
